5602c31caa4718c75ea2cbec5db1ad6bb1e2cbba
Mohamed Sayed <MohamedSayed993@hotmail.com>
2024-09-30 11:42:47 +0300

    Initial Commit

---
+++ TrendingMovies/TrendingMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingMoviesApp.swift
+//  TrendingMovies
+//
+//  Created by Mohamed Sayed on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrendingMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
